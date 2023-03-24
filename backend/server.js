9d38f047c3ba980a1dbec411b633ec943230e4b1
Akash Drolia <itcakash9@gmail.com>
2023-03-24 15:12:08 +0530

    6th commit

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const notes = require("./Data/notes");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
 dotenv.config();
@@ -11,12 +13,10 @@ app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
+app.use("/api/users", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
-
-app.get("/api/notes/:id", (req, res) => {
-  const note = notes.find((n) => n._id === req.params.id);
-
-  res.send(note);
-});
 
 app.listen(PORT, console.log(`server started listening at port ${PORT}`));
